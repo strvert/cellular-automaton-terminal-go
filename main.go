09c4958bb9d55df1b9ce4a7bead30d5e48738504
strvworks <strv13570@gmail.com>
2019-01-26 03:04:37 +0900

    create STOP, RUN, SPEEDSETTING

--- main.go
+++ main.go
@@ -8,15 +8,21 @@ import (
     "github.com/nsf/termbox-go"
 
     "github.com/strvworks/cellular-automaton-terminal-go/chunk"
-//    "github.com/strvworks/cellular-automaton-terminal-go/cellutil"
     "github.com/strvworks/cellular-automaton-terminal-go/graphic"
 )
 
 const CELL_STR = "██"
 
+type RunState int
+const (
+    RUN = iota
+    STOP
+)
+
 func main() {
     var ch chunk.Chunk
     updateInterval := 500
+
 
     for i := 0; i < 10; i++ {
         ch.SetCell(i+3, 3, 1)
@@ -40,8 +46,10 @@ func main() {
         }
     }
 
-    go updaterFunc()
+    graphic.DrawChunk(&ch, CELL_STR)
+    termbox.SetInputMode(termbox.InputEsc | termbox.InputMouse)
 
+    runstate := STOP
 MAINLOOP:
     for {
         switch ev := termbox.PollEvent(); ev.Type {
@@ -51,23 +59,46 @@ MAINLOOP:
                 ticker.Stop()
                 break MAINLOOP
 
+            case termbox.KeySpace:
+                if runstate == STOP {
+                    ticker = time.NewTicker(time.Millisecond * time.Duration(updateInterval))
+                    go updaterFunc()
+                    runstate = RUN
+                } else if runstate == RUN {
+                    ticker.Stop()
+                    runstate = STOP
+                }
+
             case termbox.KeyArrowUp:
                 if updateInterval-50 <= 0 {
                     continue
                 }
                 updateInterval -= 50
-                ticker.Stop()
-                ticker = time.NewTicker(time.Millisecond * time.Duration(updateInterval))
-                go updaterFunc()
+                if runstate == RUN {
+                    ticker.Stop()
+                    ticker = time.NewTicker(time.Millisecond * time.Duration(updateInterval))
+                    go updaterFunc()
+                }
 
             case termbox.KeyArrowDown:
                 if updateInterval+50 <= 0 {
                     continue
                 }
                 updateInterval += 50
-                ticker.Stop()
-                ticker = time.NewTicker(time.Millisecond * time.Duration(updateInterval))
-                go updaterFunc()
+                if runstate == RUN {
+                    ticker.Stop()
+                    ticker = time.NewTicker(time.Millisecond * time.Duration(updateInterval))
+                    go updaterFunc()
+                }
+            }
+        case termbox.EventMouse:
+            switch ev.Key {
+            case termbox.MouseLeft:
+                mx := ev.MouseX
+                my := ev.MouseY
+                cx := (mx/len([]rune(CELL_STR)))
+                graphic.DrawBottomMessage(fmt.Sprintf("%d, %d", mx, my), 1, 0)
+                graphic.DrawBottomMessage(fmt.Sprintf("%d, %d", cx, my), 1, 0)
             }
         }
     }
