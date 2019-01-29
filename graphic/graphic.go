c0f6372cf5ba54da141764622cae733106371cde
strvworks <strv13570@gmail.com>
2019-01-29 15:15:05 +0900

    change numeric value to constant

--- graphic/graphic.go
+++ graphic/graphic.go
@@ -6,6 +6,8 @@ import (
     "../chunkcontroller"
 )
 
+const CHUNK_SIZE = 64
+
 const coldef = termbox.ColorDefault
 
 type ScreenField struct {
@@ -16,8 +18,8 @@ type ScreenField struct {
 }
 
 func CalcDrawChunkNum(w, h int) (int, int) {
-    cx := (w / 64) + 1
-    cy := (h / 64) + 1
+    cx := (w / CHUNK_SIZE) + 1
+    cy := (h / CHUNK_SIZE) + 1
     return cx, cy
 }
 
@@ -34,7 +36,7 @@ func DrawField(cc *chunkcontroller.Chunkcontroller, field ScreenField, cellStr s
             if err != nil {
                 return 0, err
             }
-            err = DrawChunk(currch, [2]int{chx*64+ceoffX, chy*64+ceoffY}, cellStr)
+            err = DrawChunk(currch, [2]int{chx*CHUNK_SIZE+ceoffX, chy*CHUNK_SIZE+ceoffY}, cellStr)
             if err != nil {
                 return 0, err
             }
@@ -44,9 +46,9 @@ func DrawField(cc *chunkcontroller.Chunkcontroller, field ScreenField, cellStr s
 }
 
 func DrawChunk(c *chunk.Chunk, celloffset [2]int, cellStr string) error {
-    for y := 0; y < 64; y++ {
+    for y := 0; y < CHUNK_SIZE; y++ {
         offsetX := 0
-        for x := 0; x < 64; x++ {
+        for x := 0; x < CHUNK_SIZE; x++ {
             cell, err := c.GetCell(x, y)
             if err != nil {
                 return err
